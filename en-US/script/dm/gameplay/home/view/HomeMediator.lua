a06e627605c7e82ec78d3e2dfd1af6d871e3dfe5
nobbyfix <nobbyfix@users.noreply.github.com>
2022-10-28 12:34:02 +0200

    [en-US] GAME: 12552

--- en-US/script/dm/gameplay/home/view/HomeMediator.lua
+++ en-US/script/dm/gameplay/home/view/HomeMediator.lua
@@ -3634,6 +3634,7 @@ function HomeMediator:checkExtraRedPoint()
 	end
 
 	extActBtn:setPositionX(chargeBtn:getPositionX())
+	extActBtn:setVisible(false)
 
 	if chargeBtn:isVisible() then
 		extActBtn:setPositionX(chargeBtn:getPositionX() - 120)
